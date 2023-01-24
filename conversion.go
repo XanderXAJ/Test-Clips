52a514e275da03b92d8494993b91069c61543b3f
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-01-24 15:35:06 +0000

    Move generic file handling operations to file.go

--- conversion.go
+++ conversion.go
@@ -9,7 +9,6 @@ import (
 	"os/exec"
 	"os/signal"
 	"strconv"
-	"strings"
 )
 
 func conversionPossible(f flags) error {
@@ -124,40 +123,4 @@ func cleanupFailedConversion(f flags) error {
 	}
 
 	return nil
-}
-
-// generateFailedPath inserts an indicator in to originalPath to indicate failure.
-// The intention is that moving the file located at originalPath to the path returned by generateFailedPath would indicate
-// to a user that processing for that file has failed without needing to inspect the file or command output.
-func generateFailedPath(originalPath string) string {
-	return appendToFileName(originalPath, ".failed")
-}
-
-// appendtoFilename appends a string suffix to the filename, before any extension, defined by a dot.
-// For example, appending suffix "-suffix" to path "filename.ext" will result in "filename-suffix.ext".
-func appendToFileName(path string, suffix string) string {
-	originalExt := fullExt(path)
-	insertionIndex := len(path) - len(originalExt)
-	return strings.Join([]string{
-		path[:insertionIndex],
-		suffix,
-		path[insertionIndex:],
-	}, "")
-}
-
-// fullExt returns the full file name extension used by the path.
-// The full extension is the suffix beginning after the first dot in the final element of the path.
-// An empty string is returned if there is no dot.
-func fullExt(path string) string {
-	extIndex := -1
-	for i := len(path) - 1; i >= 0 && !os.IsPathSeparator(path[i]); i-- {
-		if path[i] == '.' {
-			extIndex = i
-		}
-	}
-	if extIndex != -1 {
-		return path[extIndex:]
-	} else {
-		return ""
-	}
 }
